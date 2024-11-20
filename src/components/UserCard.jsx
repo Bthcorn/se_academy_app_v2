f51a85f6af7aac91300cd8b7f7dfa1263af32cd6
Bthcorn <bowornthat.maize@gmail.com>
2024-11-21 02:21:00 +0700

    fix: user card and fetch user image

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const UserCard = ({ prop }) => {
+const UserCard = ({ prop, image, rank }) => {
   return (
-    <div className="flex h-auto w-full flex-wrap gap-4 rounded-md border bg-secondary-color4/50 p-2 duration-200 hover:scale-105 md:w-60">
+    <div className="h-auto w-full flex-wrap gap-4 rounded-md border bg-secondary-color4/50 p-2 duration-200 hover:scale-105 md:w-60">
       <div className="relative">
         <img
-          src="https://avatar.iran.liara.run/public/42"
+          src={image || "https://avatar.iran.liara.run/public/42"}
           alt="Avatar"
           className="h-16 w-16 rounded-full"
         />
@@ -15,7 +15,7 @@ const UserCard = ({ prop }) => {
       </div>
       <div className="flex flex-col items-start">
         <ul className="list-inside list-disc">
-          <li className="text-xs font-bold text-primary">Rank: 1</li>
+          <li className="text-xs font-bold text-primary">Rank: {rank}</li>
           <li className="text-xs text-foreground">Score: {prop.score}</li>
           <li className="text-xs text-foreground">Courses: 5</li>
           <li className="text-xs text-foreground">Level: {prop.level}</li>
